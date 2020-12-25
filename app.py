af324aabebbedf16df43d379c245ad20e3711dde
HarshitPathak009 <harshitpathak009@gmail.com>
2020-12-25 17:11:54 +0530

    Will not work

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+from user import Artists as Artists
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///artist.db"
@@ -9,7 +9,23 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    artists = Artists.query.order_by(Artists.id)
+    return render_template("base.html",artists=artists)
+
+
+@app.route("/admin/", methods=["POST","GET"])
+def admin():
+    if request.method == "POST":
+        artist_name = request.form['name']
+        new_artist = Artists(name=artist_name)
+
+        try:
+            db.session.add(new_artist)
+            db.session.commit()
+            return redirect(url_for('home'))
+        except:
+            return "Error adding the Artist"
+    return render_template("admin.html")
 #driver code
 if(__name__=="__main__"):
     app.run(debug=True)
